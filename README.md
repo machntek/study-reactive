dc73fc14aa3d7889d412b258f6cc6a866fa7648d
machntek <machntek@gmail.com>
2021-10-27 00:24:07 +0900

    observer 패턴 1

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 // Observer Pattern
 // Reactive Streams - 표준 - Java9 API
 
+
+### Iterator
 Java의 forEach는 Iterable을 구현한 것을 넣는다. 
 Iterable은 Iterator를 리턴하는 함수로 구현한다.
 
-Itera
+### Observable
+Observable  // Source가 -> (Event/Data)가 발생하면 -> Observer에게 알림
+
+옵저버패턴의 장점 : Observable이 뭔가(데이터/이벤트)를 만들어 내면, 이에 관심있어 하는 옵저버에게 브로드캐스트/멀티캐스트 가능
+여러개의 옵저버가 동시에 한꺼번에 이벤트를 받을 수 있다.
+
+옵저버블을 별도의 쓰레드에서 비동기적으로 동작하도록 시킴. 그러면 메인쓰레드 혹은 다른 쓰레드에서 동작하는 옵저버들이 결과를 한꺼번에 받아옴
+(push방식으로 옵저버 패턴을 사용하면 별개의 쓰레드에서 동작하는 코드를 손쉽게 만들 수 있다.)
+
--- src/main/java/com/machntek/reactive/live/Ob.java
+++ src/main/java/com/machntek/reactive/live/Ob.java
@@ -1,31 +1,66 @@
 package com.machntek.reactive.live;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+@SuppressWarnings("deprecation")
 public class Ob {
     // Iterable <---> Observable (duality) 쌍대성
     // Pull           Push
+
+    /**
+     Iterable<Integer> iter = () ->
+     new Iterator<>() {
+     int i = 0;
+     final static int MAX = 10;
+
+     public boolean hasNext() {
+     return i < MAX;
+     }
+
+     public Integer next() {
+     return ++i;
+     }
+     };
+
+     for (Integer integer : iter) {      // for-each
+     System.out.println(integer);
+     }
+     */
+
+    // 데이터만드는 소스쪽
+    static class IntObservable extends Observable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 1; i <=10; i++) {
+                setChanged();
+                notifyObservers(i);     // push
+                // int i = it.next();   // pull
+            }
+        }
+    }
+    // DATA method(void) <-> void method(DATA)
+
     public static void main(String[] args) {
 
-        Iterable<Integer> iter = () ->
-            new Iterator<>() {
-                int i = 0;
-                final static int MAX = 10;
+        // 데이터 받는쪽
+        Observer ob = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(Thread.currentThread().getName() + " " + arg);
+            }
+        };
 
-                public boolean hasNext() {
-                    return i < MAX;
-                }
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
 
-                public Integer next() {
-                    return ++i;
-                }
-            };
+        ExecutorService es = Executors.newSingleThreadExecutor();
 
-//        Iterable<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        for (Integer integer : iter) {      // for-each
-            System.out.println(integer);
-        }
+        es.execute(io);
+
+        System.out.println(Thread.currentThread().getName() + "  EXIT");
+        es.shutdown();
     }
 }
