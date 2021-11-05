2403d978db475997679b32b8244b4c070f5d7d45
machntek <machntek@gmail.com>
2021-11-05 23:09:58 +0900

    2강 - 익명클래스를 DelegateSub 클래스로 추출

--- src/main/java/com/machntek/reactive/live2/PubSub.java
+++ src/main/java/com/machntek/reactive/live2/PubSub.java
@@ -36,25 +36,10 @@ public class PubSub {
         return new Publisher<Integer>() {
             @Override
             public void subscribe(Subscriber<? super Integer> sub) {
-                pub.subscribe(new Subscriber<Integer>() {
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        sub.onSubscribe(s);
-                    }
-
+                pub.subscribe(new DelegateSub(sub) {
                     @Override
                     public void onNext(Integer i) {
                         sub.onNext(f.apply(i));
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        sub.onError(t);
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        sub.onComplete();
                     }
                 });
             }
