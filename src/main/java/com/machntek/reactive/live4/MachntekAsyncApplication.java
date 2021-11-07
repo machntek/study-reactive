bfdf1f3bcc3b82c64a10bc2c4caf967c4c31b556
machntek <machntek@gmail.com>
2021-11-07 18:18:13 +0900

    4강 - @Async 사용

---
+++ src/main/java/com/machntek/reactive/live4/MachntekAsyncApplication.java
@@ -0,0 +1,54 @@
+package com.machntek.reactive.live4;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Future;
+
+// 장시간 수행되는 작업에 대한 결과를 리턴하는 방법
+// 1. 결과를 DB에 넣고, 클라이언트가 궁금할때 DB 액세스
+// 2. Future라는 핸들러를 세션에 저장하고 바로 리턴. 궁금하면 다음 컨트롤러 메소드에서, 세션에서 Future값 꺼내서, isDone() 으로 결과 나왔는지 확인
+// 위 2개는 아주 예전(오래된) 해결책.
+
+@EnableAsync
+@SpringBootApplication
+@Slf4j
+public class MachntekAsyncApplication {
+
+    @Component
+    public static class MyService {
+        @Async
+        public Future<String> hello() throws InterruptedException {
+            log.info("hello()");
+            Thread.sleep(2000);
+            return new AsyncResult<>("Hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext c = SpringApplication.run(MachntekAsyncApplication.class, args)) {
+
+        }
+    }
+
+    @Autowired MyService myService;
+
+    @Bean
+    ApplicationRunner run() {
+        return args -> {
+            log.info("run()");
+            Future<String> f = myService.hello();
+            log.info("exit: " + f.isDone());
+            log.info("result: " + f.get());
+        };
+    }
+}
