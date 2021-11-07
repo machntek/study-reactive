124c562394b222d4704c6aba1b3dd0f35da18c0f
machntek <machntek@gmail.com>
2021-11-07 16:52:45 +0900

    4강 - Future 사용

---
+++ src/main/java/com/machntek/reactive/live4/FutureEx.java
@@ -0,0 +1,29 @@
+package com.machntek.reactive.live4;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class FutureEx {
+
+    // 비동기작업 처리 방법 - Future or callback
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Future<String> f = es.submit(() -> {
+            Thread.sleep(2000);
+            log.info("Async");
+            return "Hello";
+        });
+
+        System.out.println(f.isDone());
+        Thread.sleep(2100);
+        log.info("Exit");
+        System.out.println(f.isDone());
+        System.out.println(f.get());
+    }
+}
