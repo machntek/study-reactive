745e20f50b49c751c90d3d869eed0a653f17e55f
machntek <machntek@gmail.com>
2021-11-07 20:06:44 +0900

    4강 - 서블릿 스레드 설명

--- README.md
+++ README.md
@@ -16,3 +16,30 @@ Observable  // Source가 -> (Event/Data)가 발생하면 -> Observer에게 알�
 옵저버블을 별도의 쓰레드에서 비동기적으로 동작하도록 시킴. 그러면 메인쓰레드 혹은 다른 쓰레드에서 동작하는 옵저버들이 결과를 한꺼번에 받아옴
 (push방식으로 옵저버 패턴을 사용하면 별개의 쓰레드에서 동작하는 코드를 손쉽게 만들 수 있다.)
 
+## Servlet 3.0
+### 쓰레드가 블록킹되면 생기는 일
+쓰레드가 컨텍스트스위칭 되는 것은 CPU자원을 많이 잡아먹음.
+
+블록킹 되는 순간에 쓰레드가 waiting상태로 빠지고, 다시 정상적인 running 쓰레드가 될때 컨텍스트 스위칭 -> 블록킹 한번에 컨텍스트스위칭 2번 발생
+그래서 블록킹IO를 많이 사용하면 CPU자원이 불필요하게 많이 소모됨.
+
+### 쓰레드를 많이 만들면?
+쓰레드 하나하나가 stackTrace와 자기 데이터를 갖고있기 때문에 무한정 만들다보면 OOM 에러 발생
+
+쓰레드를 많이 만들면 context switching이 엄청 많이 발생하면서 cpu에 부하가 점점 커짐 -> 레이턴시 안좋아지고 처리율 떨어짐
+
+### http요청을 처리하는 서블릿 스레드 동작방식
+1           ST1 - req -> WorkThread -> res(html)
+2           ST2 - req -> blocking IO(DB, API) -> res(html)
+3   NIO     ST3
+4           ST4
+5           ST5
+
+### 예시
+
+INFO 3851 --- [nio-8080-exec-1] c.m.r.live4.MachntekAsyncApplication     : callable  // 서블릿스레드는 비동기작업을 리턴하고 바로 반납
+
+INFO 3851 --- [         task-1] c.m.r.live4.MachntekAsyncApplication     : async // 비동기작업을 수행하는 워커스레드. 작업이 끝나면 서블릿 스레드를 할당받아서 빠르게 응답하고 다시 반납.
+
+-> 서블릿 스레드의 가용성이 높아짐(thorughput 향상)
+
--- src/main/java/com/machntek/reactive/live4/MachntekAsyncApplication.java
+++ src/main/java/com/machntek/reactive/live4/MachntekAsyncApplication.java
@@ -13,56 +13,41 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 // 장시간 수행되는 작업에 대한 결과를 리턴하는 방법
 // 1. 결과를 DB에 넣고, 클라이언트가 궁금할때 DB 액세스
 // 2. Future라는 핸들러를 세션에 저장하고 바로 리턴. 궁금하면 다음 컨트롤러 메소드에서, 세션에서 Future값 꺼내서, isDone() 으로 결과 나왔는지 확인
 // 위 2개는 아주 예전(오래된) 해결책.
+//
+// InputStream/OutputStream 은 blocking 방식
+// HttpServletRequest/Response 는 InputStream 베이스다.
+// 따라서 서블릿은 Blokcing IO 방식이다.
 
 @EnableAsync
 @SpringBootApplication
 @Slf4j
 public class MachntekAsyncApplication {
 
-    @Component
-    public static class MyService {
-        @Async
-        public ListenableFuture<String> hello() throws InterruptedException {
-            log.info("hello()");
-            Thread.sleep(2000);
-            return new AsyncResult<>("Hello");
+    @RestController
+    public static class MyController {
+        @GetMapping("/callable")
+        public Callable<String> callable() throws InterruptedException {
+            log.info("callable");
+            return () -> {
+                log.info("async");
+                TimeUnit.SECONDS.sleep(2);
+                return "hello";
+            };
         }
-    }
-
-    @Bean
-    ThreadPoolTaskExecutor tp() {
-        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
-        te.setCorePoolSize(10);
-        te.setMaxPoolSize(100);
-        te.setQueueCapacity(50);
-        te.setThreadNamePrefix("mythread");
-        te.initialize();
-        return te;
     }
 
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext c = SpringApplication.run(MachntekAsyncApplication.class, args)) {
-
-        }
-    }
-
-    @Autowired MyService myService;
-
-    @Bean
-    ApplicationRunner run() {
-        return args -> {
-            log.info("run()");
-            ListenableFuture<String> f = myService.hello();
-            f.addCallback(s -> System.out.println(s), e -> System.out.println(e.getMessage()));
-
-            log.info("exit");
-        };
+        SpringApplication.run(MachntekAsyncApplication.class, args);
     }
 }
