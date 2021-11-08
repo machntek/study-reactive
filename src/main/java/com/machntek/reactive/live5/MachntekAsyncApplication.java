f2f0b84c4fa3c162f56a9bdf33bb3fee32ddd725
machntek <machntek@gmail.com>
2021-11-08 23:23:51 +0900

    5강 - AsyncRestTemplate에 NIO 사용(NIO쓰레드 하나로 외부api요청)

--- src/main/java/com/machntek/reactive/live5/MachntekAsyncApplication.java
+++ src/main/java/com/machntek/reactive/live5/MachntekAsyncApplication.java
@@ -1,8 +1,10 @@
 package com.machntek.reactive.live5;
 
+import io.netty.channel.nio.NioEventLoopGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,11 @@ public class MachntekAsyncApplication {
 
     @RestController
     public static class MyController {
-        AsyncRestTemplate rt = new AsyncRestTemplate();
+        // Netty가 기본적으로 call할때 쓰레드 만드는 갯수 : 프로세스 갯수 * 2개
+        AsyncRestTemplate rt = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
 
         @GetMapping("/rest")
-        public ListenableFuture<ResponseEntity<String>> rest(String idx) {
+        public ListenableFuture<ResponseEntity<String>> rest(int idx) {
             return rt.getForEntity("http://localhost:8081/service?req={req}", String.class, "hello" + idx);
         }
     }
