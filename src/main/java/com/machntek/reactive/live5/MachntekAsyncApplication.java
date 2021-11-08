91a38959957755b7554721d4360ec58a56dbc4a0
machntek <machntek@gmail.com>
2021-11-08 23:58:33 +0900

    5강 - 의존성 있는 외부 API 2개 비동기로 처리

--- src/main/java/com/machntek/reactive/live5/MachntekAsyncApplication.java
+++ src/main/java/com/machntek/reactive/live5/MachntekAsyncApplication.java
@@ -26,7 +26,13 @@ public class MachntekAsyncApplication {
             DeferredResult<String> dr = new DeferredResult<>();
             ListenableFuture<ResponseEntity<String>> f1 = rt.getForEntity("http://localhost:8081/service?req={req}", String.class, "hello" + idx);
             f1.addCallback(s -> {
-                dr.setResult(s.getBody() + "/work");
+
+                ListenableFuture<ResponseEntity<String>> f2 = rt.getForEntity("http://localhost:8081/service2?req={req}", String.class, "hello" + s.getBody());
+                f2.addCallback(s2 -> {
+                    dr.setResult(s2.getBody());
+                }, e -> {
+                    dr.setErrorResult(e.getMessage());
+                });
             }, e-> {
                 dr.setErrorResult(e.getMessage());
             });
--- src/main/java/com/machntek/reactive/live5/RemoteService.java
+++ src/main/java/com/machntek/reactive/live5/RemoteService.java
@@ -15,6 +15,12 @@ public class RemoteService {
             Thread.sleep(2000);
             return req + "/service";
         }
+
+        @GetMapping("/service2")
+        public String service2(String req) throws InterruptedException {
+            Thread.sleep(2000);
+            return req + "/service2";
+        }
     }
 
     public static void main(String[] args) {
