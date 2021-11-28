68630473145f9aa69bcf397a7b5aee64f7ca565a
machntek <machntek@gmail.com>
2021-11-29 00:08:49 +0900

    7강 - CompletableFuture async함수와 쓰레드풀 사용

--- src/main/java/com/machntek/reactive/live7/CFuture.java
+++ src/main/java/com/machntek/reactive/live7/CFuture.java
@@ -2,10 +2,7 @@ package com.machntek.reactive.live7;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Slf4j
 public class CFuture {
@@ -21,23 +18,25 @@ public class CFuture {
 //        f.completeExceptionally(new RuntimeException()); // 완료가 되긴 했는데 예외가 발생했기 때문에 완료가 된 상황이다 라는 의미를 담음. 예외가 발생했다라는 사실을 담고있을 뿐임. 누군가에게 노출하는건 아님.
 //        System.out.println(f.get()); // 이때 예외가 던져짐
 
+        // 쓰레드풀 예시
+        ExecutorService es = Executors.newFixedThreadPool(10);
+
         // 자바7부터는 풀을 아무것도 설정하지 않으면 ForkJoinPool의 commonPool이라는거의 worker가 동작함.
         CompletableFuture
                 .supplyAsync(() -> {
                     log.info("runAsync");
-                    if (1==1) throw new RuntimeException();
                     return 1;
-                })
+                }, es)
                 .thenCompose(s -> { // Function의 리턴타입이 CompletableFuture인 경우, thenCompose 사용하면 됨.(thenApply가 Stream.map이면 thenCompose는 Stream.flatMap과 비슷)
                     log.info("thenApply {}", s);
                     return CompletableFuture.completedFuture(s + 1); // CompletableFuture를 리턴하는 케이스는 추후 설명.
                 })
-                .thenApply(s2 -> {
+                .thenApplyAsync(s2 -> {
                     log.info("thenApply {}", s2);
                     return s2 * 3;
-                })
+                }, es)
                 .exceptionally(e -> -10) // 값을 복구
-                .thenAccept(s3 -> log.info("thenAccept {}", s3));
+                .thenAcceptAsync(s3 -> log.info("thenAccept {}", s3), es);
         log.info("exit");
 
         ForkJoinPool.commonPool().shutdown();
