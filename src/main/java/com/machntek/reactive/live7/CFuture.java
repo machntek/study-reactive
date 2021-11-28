860fe5a0c8fb9f581d5dc2bbd8217b745367e0bf
machntek <machntek@gmail.com>
2021-11-28 23:21:41 +0900

    7강 - CompletableFuture 비동기 쓰레드 수행(runAsync) 설명

---
+++ src/main/java/com/machntek/reactive/live7/CFuture.java
@@ -0,0 +1,34 @@
+package com.machntek.reactive.live7;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class CFuture {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//                                                        // Future 오브젝트를 만듦(완료된 상태)
+//        CompletableFuture<Integer> f = CompletableFuture.completedFuture(1);
+
+//        CompletableFuture<Integer> f = new CompletableFuture<>(); // 아직 작업이 완료되지 않은 Future
+//        f.complete(2);  // 작업이 완료된 Future
+//        System.out.println(f.get());
+
+//        CompletableFuture<Integer> f = new CompletableFuture<>();
+//        f.completeExceptionally(new RuntimeException()); // 완료가 되긴 했는데 예외가 발생했기 때문에 완료가 된 상황이다 라는 의미를 담음. 예외가 발생했다라는 사실을 담고있을 뿐임. 누군가에게 노출하는건 아님.
+//        System.out.println(f.get()); // 이때 예외가 던져짐
+
+        // 자바7부터는 풀을 아무것도 설정하지 않으면 ForkJoinPool의 commonPool이라는거의 worker가 동작함.
+        CompletableFuture
+                .runAsync(() -> log.info("runAsync"))
+                .thenRun(() -> log.info("thenRun"))
+                .thenRun(() -> log.info("thenRun2"));
+        log.info("exit");
+
+        ForkJoinPool.commonPool().shutdown();
+        ForkJoinPool.commonPool().awaitTermination(10, TimeUnit.SECONDS);
+    }
+}
