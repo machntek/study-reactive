2b875091670ccaa20097de4779b56e9ab78dde15
machntek <machntek@gmail.com>
2021-12-13 00:02:46 +0900

    10강 - Mono와 Flux 비교/차이 1

--- src/main/java/com/machntek/reactive/live10/MachntekApplication.java
+++ src/main/java/com/machntek/reactive/live10/MachntekApplication.java
@@ -11,19 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @Slf4j
 @RestController
 public class MachntekApplication {
 
     @GetMapping("/event/{id}")
-    Mono<Event> event(@PathVariable long id) {
-        return Mono.just(new Event(id, "event" + id));
+    Mono<List<Event>> event(@PathVariable long id) {
+        List<Event> list = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
+        return Mono.just(list); // List타입 자체가 하나의 element임.
     }
 
     @GetMapping("/events")
     Flux<Event> events() {
-        return Flux.just(new Event(1L, "event1"), new Event(2L, "event2"));
+        List<Event> list = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
+        return Flux.fromIterable(list); // 각 element에 map 등의 reactor 라이브러리의 오퍼레이터 작업 걸 수 있다.
     }
 
     public static void main(String[] args) {
