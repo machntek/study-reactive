e226740c166bde8fea7ba3d34cd5aa483a31668c
machntek <machntek@gmail.com>
2021-12-13 21:44:04 +0900

    10강 - Stream으로 Flux 응답 스트림 리턴

--- src/main/java/com/machntek/reactive/live10/MachntekApplication.java
+++ src/main/java/com/machntek/reactive/live10/MachntekApplication.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @Slf4j
@@ -28,8 +30,10 @@ public class MachntekApplication {
 
     @GetMapping(value="/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<Event> events() {
-        List<Event> list = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
-        return Flux.fromIterable(list); // 각 element에 map 등의 reactor 라이브러리의 오퍼레이터 작업 걸 수 있다.
+        return Flux
+                .fromStream(Stream.generate(() -> new Event(System.currentTimeMillis(), "value")))  // Stream.limit()으로 제한 걸 수도 있다.
+                .delayElements(Duration.ofSeconds(1))
+                .take(10); // 각 element에 map 등의 reactor 라이브러리의 오퍼레이터 작업 걸 수 있다.
     }
 
     public static void main(String[] args) {
